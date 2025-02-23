3ebb243a5d7ca1a2df0aeca720dde0152c9235fc
JoshMBN <joshua.brennan.niu@gmail.com>
2025-02-23 19:16:25 +1300

    Add initial implementation of Todo menu using inquirer

---
+++ main.py
@@ -0,0 +1,33 @@
+import inquirer
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')    
+
+
+# Create a menu using inquirer
+questions = [
+    inquirer.List('action',
+                    message="What do you want to do?",
+                    choices=['Create a new Todo', 'Delete a Todo', 'Update a Todo', 'List all Todos'],
+                ),
+]
+
+answers = inquirer.prompt(questions)
+
+if answers['action'] == 'Create a new Todo':
+    clear()
+    print('Creating a new Todo')
+elif answers['action'] == 'Delete a Todo':
+    clear()
+    print('Deleting a Todo')
+elif answers['action'] == 'Update a Todo':
+    clear()
+    print('Updating a Todo')
+elif answers['action'] == 'List all Todos':
+    clear()
+    print('Listing all Todos')
+else:
+    clear()
+    print('Invalid option')
+
